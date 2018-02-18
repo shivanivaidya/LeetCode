06411536caf4142619c6dad26b9fd79abbeee3b7
shivani vaidya <shivani@onlab.us>
2018-02-18 00:34:18 -0500

    Added solution to problem1.

---
+++ Easy/Problem1.java
@@ -0,0 +1,29 @@
+/* 
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+*/
+
+class Problem1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i<nums.length; i++) {
+            if(map.containsKey(target-nums[i])) {
+                int[] soln = {map.get(target-nums[i]), i};
+                return soln;
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
